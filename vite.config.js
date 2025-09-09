5103410ef9aaa2635c4d7f60422888e1013267dc
Hebaeid1983 <heba.eid.atia@gmail.com>
2025-09-08 17:23:29 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/karmelsham/', // <--- IMPORTANT for GitHub Pages
   plugins: [react()],
-  base: "/Heba-eid-Atia-karam-elsham/", 
 });
