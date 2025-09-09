f5337c650f81b9a4b75f91c4b0d73b522f2ce520
Hebaeid1983 <your-email@example.com>
2025-09-09 10:59:47 -0700

    Updates

--- src/assets/menuData.js
+++
@@ -1,8 +0,0 @@
-export const highlights = [
-  { id: 1, name: "شاورما عربي دجاج", price: 135, img: "https://images.unsplash.com/photo-1612872087720-bb876e2e67e3?q=80&w=1600&auto=format&fit=crop" },
-  { id: 2, name: "شاورما عربي لحم", price: 165, img: "https://images.unsplash.com/photo-1586816001966-79b736744398?q=80&w=1600&auto=format&fit=crop" },
-  { id: 3, name: "شاورما عربي دبل دجاج", price: 210, img: "https://images.unsplash.com/photo-1580656289681-5f9f8d9ed2c0?q=80&w=1600&auto=format&fit=crop" },
-  { id: 4, name: "صينية مكس شاورما", price: 400, img: "https://images.unsplash.com/photo-1604908554152-67a0b6c113c3?q=80&w=1600&auto=format&fit=crop" },
-  { id: 5, name: "بوكس المعلم ميكس فرنسـاوي", price: 370, img: "https://images.unsplash.com/photo-1541516160071-3b6b319fdece?q=80&w=1600&auto=format&fit=crop" },
-  { id: 6, name: "مشروب لتر", price: 25, img: "https://images.unsplash.com/photo-1551024601-bec78aea704b?q=80&w=1600&auto=format&fit=crop" },
-]
